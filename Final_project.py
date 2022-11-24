c387729b100e12df056def2e9193a72a502974c2
htchanax <75979068+htchanax@users.noreply.github.com>
2022-11-24 11:38:56 +0800

    Update Final_project.py

--- Final_project.py
+++ Final_project.py
@@ -1,15 +1,9 @@
 # Assignment: Final Project - main execution file importing codes from other programs
-
-# Part1 - Shortcake Program(60%) - main execution program, importing shortcakes.py (class defining program)
-# Description: Mrs.Bake owns a small bakery selling four different types of shortcakes
-# She would like to have a repeatedly-used business system built
-
+# Shortcake Program - main execution program, importing shortcakes.py (class defining program)
+# Description: Mrs.Bake owns a small bakery selling four different types of shortcakes, repeatedly-used business system built
 # Learning Objectives: Creating two classes, applying inheritance and polymorphism
 
-
 import shortcakes
-
-
 
 def main():
     try:
@@ -118,3 +112,61 @@ def getShortcake(list):
 
 #calling the main function to execute    
 main()
+# Assignment: Final Project - Program of class (shortcakes.py) - defining class
+# Shortcake Program - defining classes program
+# Description: Mrs.Bake owns a small bakery selling four different types of shortcakes，repeatedly-used business system built
+# Learning Objectives: Creating two classes, applying inheritance and polymorphism
+
+# superclass program
+
+class Shortcake:
+
+    def __init__(self,price,flavor,inventory):
+        self.__price = price
+        self.__flavor = flavor
+        self.__inventory = inventory
+
+    def __str__(self):
+        return '\nPrice is '+str(self.__price)+'\nFlavor is '+self.__flavor+'\nInventory is '+str(self.__inventory)+'\n'
+    
+
+    def set_price(self,price):
+        self.__price = price
+
+    def set_flavor(self,flavor):
+        self.__flavor = flavor
+        
+    def set_inventory(self,inventory):
+        self.__inventory = inventory
+
+    def get_price(self):
+        return self.__price
+        
+    def get_flavor(self):
+        return self.__flavor
+        
+    def get_inventory(self):
+        return self.__inventory
+
+
+
+
+# subclass program
+
+
+class ShortcakeWithTopping(Shortcake):
+
+    def __init__(self,price,flavor,inventory,topping):
+        Shortcake.__init__(self,price,flavor,inventory)
+        self.__topping = topping
+
+    def __str__(self):
+        return Shortcake.__str__(self)+'Topping is '+self.__topping
+    
+    def set_topping(self,topping):
+        self.__topping = topping
+
+    def get_topping(self):
+        return self.__topping
+        
+
